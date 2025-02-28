efe7bc1993f23dfacebb17bbcc482ab49cc7b4c6
Victor Hugo <victr.hf@gmail.com>
2025-02-28 20:55:55 -0300

    add nodemailer

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const session = require('express-session');
+const nodemailer = require('nodemailer');
 const app = express();
 const routes = require('./routes');
 const port = 3000;
@@ -10,6 +11,33 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }));
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: "OAuth2",
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN
+    }
+});
+
+const mailOptions = {
+    from: 'vhugo.freire159@gmail.com',
+    to: 'vhugo.freire159@gmail.com',
+    subject: 'Projeto nodemailer',
+    text: 'olá, to usando nodemailer'
+}
+
+transporter.sendMail(mailOptions, function(err, data) {
+    if(err) {
+        console.log( err )
+    } else {
+        console.log("email enviado!")
+    }
+})
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,5 +49,4 @@ app.use(routes);
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
-    console.log(process.env.TESTE)
 })
